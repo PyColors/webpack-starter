819db38c579b7be7b12f9d2f07d6493602494fd3
Patrice <patweb44@gmail.com>
2018-05-25 17:54:17 +0000

    Adding NODE_ENV

--- build/webpack.dev.js
+++ build/webpack.dev.js
@@ -3,13 +3,14 @@ const webpack = require('webpack')
 
 // add compiler HTML...
 // add file for entry
-config.entry.app.unshift("./build/dev-client.js")
+config.entry.app.unshift("./webpack/dev-client.js")
 
 // Hot Module Replacement with node.js AP
 config.plugins = config.plugins.concat([
   new webpack.HotModuleReplacementPlugin(),
+  new webpack.EnvironmentPlugin(['NODE_ENV']),
 
-  // Compile if Error (webpack-hot-middleware)
+// Compile if Error (webpack-hot-middleware)
   new webpack.NoErrorsPlugin()
 ])
 
--- build/webpack.prod.js
+++ build/webpack.prod.js
@@ -6,15 +6,35 @@ const extractCSS = new ExtractTextPlugin('bundle.css');
 config.plugins = config.plugins.concat([
   extractCSS,
   new webpack.optimize.UglifyJsPlugin({
-    comments: false
+    compress: {
+      warnings: false,
+      screw_ie8: true,
+      conditionals: true,
+      unused: true,
+      comparisons: true,
+      sequences: true,
+      dead_code: true,
+      evaluate: true,
+      if_return: true,
+      join_vars: true
+    },
+    output: {
+      comments: false
+    }
+  }),
+  
+   new webpack.DefinePlugin({
+    'process.env': {
+      'NODE_ENV': JSON.stringify('production')
+    }
   })
 ])
 
-// css-loader - get css and style
-// can add in the futur some pre-processed CSS
+// Css-loader - get CSS and style
+// Can add in the futur some pre-processed CSS
 let cssLoaders = config.module.loaders[0].loaders
 config.module.loaders[0].loaders = null
-// get loader css and remove first value
-config.module.loaders[0].loader = extractCSS.extract(cssLoaders.slice(1,10))
+// Get loader CSS and remove first value
+config.module.loaders[0].loader = extractCSS.extract(cssLoaders.slice(1, 10))
 
 module.exports = config
